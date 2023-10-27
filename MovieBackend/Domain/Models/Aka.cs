7cbe4986073c448c8df1d82e078a5abd004941e2
Sunese <suneengtorp@live.dk>
2023-10-27 09:03:53 +0200

    feat(domain): add empty models

---
+++ MovieBackend/Domain/Models/Aka.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Domain.Models;
+
+public class Aka
+{
+	public string TitleID { get; set; }
+	public int OrderID { get; set; }
+}
+
---
+++ MovieBackend/Domain/Models/Name.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Domain.Models;
+
+public class Name
+{
+	public Name()
+	{
+	}
+}
+
