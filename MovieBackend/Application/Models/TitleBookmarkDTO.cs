96701d8911a0a7c2b14aae660333521be91f1bab
Sune Engtorp <suneengtorp@live.dk>
2023-10-30 19:30:48 +0100

    feat: add bookmark controller and service

---
+++ MovieBackend/Application/Models/TitleBookmarkDTO.cs
@@ -0,0 +1,7 @@
+namespace Application.Models;
+    
+public class TitleBookmarkDTO
+{
+	public string TitleId { get; set; } = String.Empty;
+    public string Notes { get; set; } = String.Empty;
+}
---
+++ MovieBackend/Application/Services/IBookmarkService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Models;
+
+namespace Application.Services;
+
+public interface IBookmarkService
+{
+    void CreateNameBookmark(string username, NameBookmarkDTO model);
+    void UpdateNameBookmark(string username, NameBookmarkDTO model);
+    void DeleteNameBookmark(string username, NameBookmarkDTO model);
+
+    void CreateTitleBookmark(string username, TitleBookmarkDTO model);
+    void UpdateTitleBookmark(string username, TitleBookmarkDTO model);
+    void DeleteTitleBookmark(string username, TitleBookmarkDTO model);
+}
