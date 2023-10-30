f3f24847987c7fa5e8c277a895e9e6e45717b7be
Jonas Hagel <53375542+Hagel360@users.noreply.github.com>
2023-10-30 14:59:20 +0100

    feat(): renaming, refactoring and more work on title part

--- MovieBackend/API/Controllers/TitleController.cs
+++ MovieBackend/API/Controllers/TitleController.cs
@@ -10,23 +10,40 @@ public class TitleController : ControllerBase
 {
 
     private readonly ILogger<TitleController> _logger;
-    private readonly IImdbService _imdbService;
+    private readonly ITitleService _titleService;
 
-    public TitleController(ILogger<TitleController> logger, IImdbService imdbService)
+    public TitleController(ILogger<TitleController> logger, ITitleService titleService)
     {
         _logger = logger;
-        _imdbService = imdbService;
+        _titleService = titleService;
     }
 
     [HttpGet]
-    public IList<TitleDTO> Get()
+    public IList<TitleDTO> Get(int startyear = 1, int startmonth = 1, int startday = 1, int endyear = 9999, int endmonth = 12, int endday = 31, int count = 10)
     {
-        return _imdbService.GetTitles(10);
+        DateTime startDate = new DateTime(startyear, startmonth, startday);
+        DateTime endDate = new DateTime(endyear, endmonth, endday);
+        return _titleService.Get(startDate, endDate, count);
     }
 
     [HttpGet("{id}")]
     public TitleDTO Get(string id)
     {
-        return _imdbService.GetTitle(id);
+        return _titleService.GetTitle(id);
+    }
+
+    // Title/GetFeature
+    [HttpGet(nameof(GetFeature))]
+    public IList<TitleDTO> GetFeature(int year = 00, int month = 00, int count = 10)
+    {
+        return _titleService.GetFeature(year, month, count);
+    }
+
+    // Title/GetPopular
+    [HttpGet(nameof(GetPopular))]
+    public IList<TitleDTO> GetPopular(int year = 1, int month = 1, int day = 1, int count = 10)
+    {
+        DateTime datetime = new DateTime(year, month, day);
+        return _titleService.GetPopular(datetime, count);
     }
 }
---
+++ MovieBackend/Application/Services/ITitleService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Models;
+using Domain.Models;
+
+namespace Application.Services;
+
+public interface ITitleService
+{
+    IList<TitleDTO> Get(DateTime startdatetime, DateTime enddatetime, int num, bool isAdult = false);
+    TitleDTO GetTitle(string id, bool isAdult = false);
+    IList<TitleDTO> GetFeature(int year, int month, int num, bool isAdult = false);
+    IList<TitleDTO> GetPopular(DateTime datetime, int num, bool isAdult = false);
+}
